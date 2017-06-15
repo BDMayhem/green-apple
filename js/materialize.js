8462f68164b13e6704fd51d7ee66633fe401033b
BDMayhem <Brianjdiaz@gmail.com>
2017-06-15 00:26:58 -0400

    Added services (#1)
    * Add files via upload
    
    Added Services page and linked to it on other pages.  Added consistent border on Contact page.
    
    * Add files via upload
    
    fixed Google Fonts link

--- js/materialize.js
+++ js/materialize.js
@@ -8321,3 +8321,25 @@ Picker.extend( 'pickadate', DatePicker )
   };
 
 }( jQuery ));
+
+
+var fullPlanning = $("#full-planning").height();
+var partialPlanning = $("#partial-planning").height();
+var eventManagement = $("#event-management").height();
+var alacarte = $("#alacarte").height();
+
+if (fullPlanning > partialPlanning) {
+	var setHeight = $("#fp-outer").height() + "px";
+	document.getElementById("partial-planning").style.height = setHeight;
+} else {
+	var setHeight = $("#pp-outer").height() + "px";
+	document.getElementById("full-planning").style.height = setHeight;
+}
+
+if (eventManagement > alacarte) {
+	var setHeight = $("#em-outer").height() + "px";
+	document.getElementById("alacarte").style.height = setHeight;
+} else {
+	var setHeight = $("#a-outer").height() + "px";
+	document.getElementById("event-management").style.height = setHeight;
+}
