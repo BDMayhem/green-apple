4034e25dbc90c597b1f56c969df745e48a312292
BDMayhem <Brianjdiaz@gmail.com>
2017-06-22 17:46:27 -0400

    Gallery update
    Added gallery page
    
    Updated services page
    
    Updated all pages to include gallery links

--- js/services.js
+++ js/services.js
@@ -1,20 +1,40 @@
-var fullPlanning = $("#full-planning").height();
-var partialPlanning = $("#partial-planning").height();
-var eventManagement = $("#event-management").height();
-var alacarte = $("#alacarte").height();
+$(document).ready(function(){
+	equalBoxes();
+});
 
-if (fullPlanning > partialPlanning) {
-	var setHeight = $("#fp-outer").height() + "px";
-	document.getElementById("partial-planning").style.height = setHeight;
-} else {
-	var setHeight = $("#pp-outer").height() + "px";
-	document.getElementById("full-planning").style.height = setHeight;
-}
+function equalBoxes(){
+	$("#full-planning").removeAttr("style");
+	$("#partial-planning").removeAttr("style");
+	$("#event-management").removeAttr("style");
+	$("#alacarte").removeAttr("style");
 
-if (eventManagement > alacarte) {
-	var setHeight = $("#em-outer").height() + "px";
-	document.getElementById("alacarte").style.height = setHeight;
-} else {
-	var setHeight = $("#a-outer").height() + "px";
-	document.getElementById("event-management").style.height = setHeight;
-}
+	if($(window).width() > 584){
+
+		var fullPlanning = $("#full-planning").height();
+		var partialPlanning = $("#partial-planning").height();
+		var eventManagement = $("#event-management").height();
+		var alacarte = $("#alacarte").height();
+
+		if (fullPlanning > partialPlanning) {
+			var setHeight = $("#fp-outer").height() + "px";
+			document.getElementById("partial-planning").style.height = setHeight;
+		} else {
+			var setHeight = $("#pp-outer").height() + "px";
+			document.getElementById("full-planning").style.height = setHeight;
+		};
+
+		if (eventManagement > alacarte) {
+			var setHeight = $("#em-outer").height() + "px";
+			document.getElementById("alacarte").style.height = setHeight;
+		} else {
+			var setHeight = $("#a-outer").height() + "px";
+			document.getElementById("event-management").style.height = setHeight;
+		};	
+	};
+
+	if($(window).width() > 1500){
+		$("div.row.center").addClass("container");
+	};
+};
+
+$(window).resize(equalBoxes);
